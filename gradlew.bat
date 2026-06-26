c5a4dd168bc83e154c47bfba0abcf7f97ea1f332
NonKungCh. <116896756+NonKun1122@users.noreply.github.com>
2026-06-26 08:38:28 +0700

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,8 +1,7 @@
 @echo off
 setlocal
 
-set APP_HOME=%\~dp0
-
-set CLASSPATH=%APP_HOME%gradle\wrapper\gradle-wrapper.jar
+set APP_HOME=%~dp0
+set CLASSPATH=%APP_HOME%gradlewrappergradle-wrapper.jar
 
 java -Xmx64m -Xms64m -Dorg.gradle.appname=gradlew -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
